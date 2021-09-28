38944257f6c4eddc8a7d1161fdd004bfa693ba86
shimomu919 <shimomu-919214@outlook.jp>
2021-09-28 15:32:29 +0900

    リポジトリ初期化

---
+++ src/test/java/oit/is/z1004/kadai/janken/JankenApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1004.kadai.janken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
